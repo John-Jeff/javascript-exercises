b5d9b9dfae20647c22517370788ee0bbc3d76157
Jeff <jeffreyjohn102@gmail.com>
2022-07-03 23:48:25 -0400

    Complete 01 to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...remove) {
+    for (let i of remove) {
+        if (array.indexOf(i) === -1) {
+            continue;
+        } else {
+            array.splice(array.indexOf(i), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    let sum = 0;
+    if (typeof(min) !== 'number' || typeof(max) !== 'number') {
+        return 'ERROR';
+    }
+    if (min < 0 || max < 0) {
+        return 'ERROR';
+    }
+    if (max < min) {
+        let newMax = min;
+        min = max;
+        max = newMax;
+    }
+    for (let i = min; i <= max; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
