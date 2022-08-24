3e5d392771ac868cdd361064c3c0d0b779d7215a
jeff <jeffreyjohn102@gmail.com>
2022-08-24 14:16:45 -0400

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let currentYear = new Date().getFullYear();
 
+    array.forEach(function(array) {
+        if (array.yearOfDeath === undefined) array.yearOfDeath = currentYear;
+    });
+
+    return array.reduce((prev, init) =>
+        (prev.yearOfDeath - prev.yearOfBirth) > (init.yearOfDeath - init.yearOfBirth) ? prev : init);
 };
 
 // Do not edit below this line
